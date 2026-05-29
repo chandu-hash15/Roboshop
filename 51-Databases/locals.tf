5caba8d73ae56a5efa4dfc79b3fefcdb31c0bc7d
Chandu <chandutharun42@gmail.com>
2026-05-29 12:07:13 +0530

    changes in locals.tf as we didn't add value at data block for database subnet

--- 51-Databases/locals.tf
+++ 51-Databases/locals.tf
@@ -8,7 +8,7 @@ locals {
         data.aws_ssm_parameter.private_subnet-2_id
     ]
 
-    database_subnet_id = data.aws_ssm_parameter.database_subnet-1_id
+    database_subnet_id = data.aws_ssm_parameter.database_subnet-1_id.value
 
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
