f32cb3dde2d964a746bfbd4735b0bb921593acdb
Chandu <chandutharun42@gmail.com>
2026-05-29 14:59:06 +0530

    enitre main file changed in databases

--- 51-Databases/bootstrap.sh
+++ 51-Databases/bootstrap.sh
@@ -6,7 +6,7 @@ component=$1
 
 dnf install ansible -y
 
-ansible-pull -U https://github.com/chandu-hash15/Terra_Ansi_Roles-_Integration.git \
-  -i localhost, \
-  -e "component=$component"\
-  main.yaml
+ansible-pull -U https://github.com/chandu-hash15/Terra_Ansi_Roles_Integration.git \
+  -i inventory.ini \
+  -e "component=$component" \
+  main.yaml -vv
