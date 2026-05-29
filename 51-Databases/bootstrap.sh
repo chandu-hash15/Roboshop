a3f1c425f4f070a67e63a5b4ffb549f2efed7392
Chandu <chandutharun42@gmail.com>
2026-05-29 12:27:56 +0530

    changes in bootstarp file

--- 51-Databases/bootstrap.sh
+++ 51-Databases/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-component =$1
+component=$1
 
 dnf install ansible -y
 
-ansible-pull -U https://github.com/chandu-hash15/Terra_Ansi_Roles-_Integration.git -e = "component=$component" main.yaml    
+ansible-pull -u https://github.com/chandu-hash15/Terra_Ansi_Roles_Integration.git -e "component=$component" main.yaml   
