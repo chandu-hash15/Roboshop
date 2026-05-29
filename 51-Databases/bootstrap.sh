147c4c9171d49dcd0a4308b9bf9049cf94651605
Chandu <chandutharun42@gmail.com>
2026-05-29 15:23:41 +0530

    made changes in sh file

--- 51-Databases/bootstrap.sh
+++ 51-Databases/bootstrap.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 component=$1
 
+echo "Installing Ansible..."
 dnf install ansible -y
 
-ansible-pull -U https://github.com/chandu-hash15/Terra_Ansi_Roles-_Integration.git \
-  -i inventory.ini \
+echo "Running ansible-pull for $component..."
+
+ansible-pull -U https://github.com/chandu-hash15/Terra_Ansi_Roles_Integration.git \
+  -i localhost, \
   -e "component=$component" \
   main.yaml -vv
 
