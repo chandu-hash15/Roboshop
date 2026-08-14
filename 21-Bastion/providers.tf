635e70c5535d3cbe519ce83579a8484c7b71397d
Chandu <chandutharun42@gmail.com>
2026-08-14 16:12:39 +0530

    changes in the procider of bastion the name of the bucket has changed

--- 21-Bastion/providers.tf
+++ 21-Bastion/providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "chathrun-myvpc-module"
+    bucket = "chathrun-bastion-module"
     key    = "chathrun-robodevbastion-module"
     region = "us-east-1"
     use_lockfile = true
