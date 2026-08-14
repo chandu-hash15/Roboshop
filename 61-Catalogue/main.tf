82065aabfb231fc21d62dd9a4565c537b92f64d3
Chandu <chandutharun42@gmail.com>
2026-08-14 13:27:46 +0530

    changes in the hosted zone id varaiables  which got changed in route53 records in 61-Catalogue

--- 61-Catalogue/main.tf
+++ 61-Catalogue/main.tf
@@ -41,3 +41,17 @@ resource "terraform_data" "catalogue" {
     ]
   }
 }
+
+# Stopping the instance to take ami
+
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on = [terraform_data.catalogue]
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "catalogue_ami"
+  source_instance_id = aws_instance.catalogue.id
+  depends_on = [aws_ec2_instance_state.catalogue]
+}
