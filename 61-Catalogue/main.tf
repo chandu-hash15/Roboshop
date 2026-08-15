3f42adb15b5a6347668aa7f843118bec75c890b8
Chandu <chandutharun42@gmail.com>
2026-08-15 11:32:40 +0530

    changes in the launch template fixed the syntax error

--- 61-Catalogue/main.tf
+++ 61-Catalogue/main.tf
@@ -118,7 +118,7 @@ resource "aws_autoscaling_group" "catalogue" {
   force_delete              = false
   launch_template {
     id      = aws_launch_template.catalogue.id
-    version = aws_launch_template.catalogue_version
+    version = aws_launch_template.catalogue.latest_version
   }
   target_group_arns         = aws_lb_target_group.catalogue.arn
   vpc_zone_identifier       = local.private_subnet_ids
